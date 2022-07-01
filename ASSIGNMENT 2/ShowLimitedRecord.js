dd96c5a70b80d3687eb825aa04679c1dfb898549
ganeshaware18 <ganeshaware18@gmail.com>
2022-07-01 13:50:26 +0530

    new opreation files added

---
+++ ASSIGNMENT 2/ShowLimitedRecord.js
@@ -0,0 +1,18 @@
+var mysql =require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "root123",
+    database: "mydb"
+  });
+
+con.connect(function(err) {
+  if (err) throw err;
+  var sql = "SELECT * FROM customers LIMIT 5";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
