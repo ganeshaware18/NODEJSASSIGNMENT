9342a8b022c6aca994d2f7c6fb84b237bda64e40
ganeshaware18 <ganeshaware18@gmail.com>
2022-07-01 13:53:44 +0530

    adding output

--- ASSIGNMENT 2/ShowLimitedRecord.js
+++ ASSIGNMENT 2/ShowLimitedRecord.js
@@ -16,3 +16,14 @@ con.connect(function(err) {
     console.log(result);
   });
 });
+
+/*
+PS D:\NODEJSASSIGNMENT\NODEJSASSIGNMENT\ASSIGNMENT 2> node "d:\NODEJSASSIGNMENT\NODEJSASSIGNMENT\ASSIGNMENT 2\ShowLimitedRecord.js"
+[
+  RowDataPacket { name: 'Company Inc', address: 'Highway 37' },
+  RowDataPacket { name: 'uality hardware', address: 'nashik 15' },
+  RowDataPacket { name: 'Amy', address: 'Apple st 652' },
+  RowDataPacket { name: 'Michael', address: 'Canyon 123' },
+  RowDataPacket { name: 'Sandy', address: 'Ocean blvd 2' }
+]
+*/
